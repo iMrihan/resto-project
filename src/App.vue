4143a34a0b422fe3ef2c2eac4a93e3247d2b46b0
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-24 15:00:16 +0530

    signup page is done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <SignUp />
 </template>
 
 <script>
+import SignUp from "./components/SignUp.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    SignUp,
+  },
 };
 </script>
 
---
+++ src/components/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <img class="logo" src="../assets/pngwing.com.png" alt="" />
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" placeholder="Enter Name" />
+
+    <input type="email" placeholder="Enter Email" />
+
+    <input type="password" placeholder="Enter password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingUp",
+};
+</script>
+
+<style>
+.logo {
+  width: 100px;
+}
+
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid skyblue;
+}
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  color: white;
+  background-color: skyblue;
+  cursor: pointer;
+}
+</style>
