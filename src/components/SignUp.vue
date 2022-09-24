0a9e2294923905567c7be62461a456eab9764da9
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-24 17:50:21 +0530

    setup json-server and created post request for signup page

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,18 +2,40 @@
   <img class="logo" src="../assets/pngwing.com.png" alt="" />
   <h1>Sign Up</h1>
   <div class="register">
-    <input type="text" placeholder="Enter Name" />
+    <input type="text" placeholder="Enter Name" v-model="name" />
 
-    <input type="email" placeholder="Enter Email" />
+    <input type="email" placeholder="Enter Email" v-model="email" />
 
-    <input type="password" placeholder="Enter password" />
-    <button>Sign Up</button>
+    <input type="password" placeholder="Enter password" v-model="password" />
+    <button v-on:click="signup">Sign Up</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "SingUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signup() {
+      let result = await axios.post("http://localhost:3000/users", {
+        email: this.email,
+        name: this.name,
+        password: this.password,
+      });
+      console.log("result", result);
+      if (result.status === 201) {
+        alert("Sign Up Done");
+      }
+      localStorage.setItem("user-info", JSON.stringify(result.data));
+    },
+  },
 };
 </script>
 
